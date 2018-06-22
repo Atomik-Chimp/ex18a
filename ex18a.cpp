7e776a56391098d76206589b79ef896bad1e154c
particle-m <particle_m@hotmail.com>
2018-06-22 15:35:07 -0800

    Testing the Github interface with Visual Studio 2010 express
    This will be my Fifth attempt

--- ex18a.cpp
+++ ex18a.cpp
@@ -1,11 +1,19 @@
-// ex18a.cpp : Defines the entry point for the console application.
-//
+/* ex18a.cpp : Defines the entry point for the console application. */
+/* */
+/* Kernighan and Ritchie, exercise 1-8 */
+/* Write a program to replace each tab by the three-character sequence >, <backspace>, -, which printrs as <arrow> */
+/* and each backspace by the similar sequence <, <backspace>, - this makes tabs and backspaces visible. */
 
-#include "stdafx.h"
+#include "stdafx.h"	/* not all C++, autowritten by Visual C++ 2010 */
 
 
-int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, char* argv[])
 {
+	int c;		/* to hold the input characters */
+	while((c = getchar()) != EOF){		/* while we have not reached the end */
+		if(c == '\t')
+	getchar();
+
 	return 0;
 }
 

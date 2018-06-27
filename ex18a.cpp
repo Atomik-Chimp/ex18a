63233e39dbae6ba18c4393af6de1badd4a1da44e
particle-m <particle_m@hotmail.com>
2018-06-27 12:12:19 -0800

    Still Figuring out files and associations
    This will be my Seventh attempt.

--- ex18a.cpp
+++ ex18a.cpp
@@ -1,8 +1,9 @@
 /* ex18a.cpp : Defines the entry point for the console application. */
 /* */
 /* Kernighan and Ritchie, exercise 1-8 */
-/* Write a program to replace each tab by the three-character sequence >, <backspace>, -, which printrs as <arrow> */
+/* Write a program to replace each tab by the three-character sequence >, <backspace>, -, which prints as <arrow> */
 /* and each backspace by the similar sequence <, <backspace>, - this makes tabs and backspaces visible. */
+/* Note: This does not work this way in Visual Studio 2010 */
 
 #include "stdafx.h"	/* autowritten by Visual C++ 2010, Adjusted to be C */
 
